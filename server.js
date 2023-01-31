3edd039aa5a534164179a6ffb06a45913a0ffd60
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-01-31 10:31:33 +0530

    nodemon

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 
 // const mongo = require("mongodb");
 
 const app = express();
+
+const MongoDBURL = "localhost:27017";
+
+mongodb: await mongoose.connect(MongoDBURL);
 
 app.listen(7000);
