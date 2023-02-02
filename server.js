b693e7c4806dae344bcae7d05d60563db2aeab79
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-02 10:04:21 +0530

    app.get("/zen", (req, res, next)

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const connectMongo = require("./utils/connection");
 
 const app = express();
 
-// const MongoDBURL = "mongodb://localhost:27017/ZenKanban";
+app.get("/zen", (req, res, next) => {
+  // imp information from the req object is as follows
+  console.log("req.url: ", req.url);
+  console.log("req.method: ", req.method);
+  console.log("req.headers ", req.headers);
+
+  res.send("");
+});
 
 connectMongo();
 app.use("/api");
 app.listen(7000);
+
+// when the path mathes, express runs the middleware
+
+// req and res are given by express in a function called as "next"
