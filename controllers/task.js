68f372e86a0dd48a9662b57e174d9c13292102f4
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-02 10:45:06 +0530

    app.use(express.urlencoded());

--- controllers/task.js
+++ controllers/task.js
@@ -2,4 +2,8 @@ const testFn = (req, res, next) => {
   res.status(200).send("Task Controller");
 };
 
-module.exports = { testFn: testFn };
+function createTask(body) {
+  res.status(200).send("We will create a task here");
+}
+
+module.exports = { testFn: testFn, createTask: createTask };
