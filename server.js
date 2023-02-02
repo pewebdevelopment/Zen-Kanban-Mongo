1b2fcec31f63294ab1520d59c23a78f3f90be702
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-02 09:59:14 +0530

    const routes = require("./routes/index.js");

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const routes = require("./routes/index.js");
+
 const express = require("express");
 
 const connectMongo = require("./utils/connection");
 
 const app = express();
 
-const MongoDBURL = "mongodb://localhost:27017/ZenKanban";
+// const MongoDBURL = "mongodb://localhost:27017/ZenKanban";
 
 connectMongo();
-
+app.use("/api");
 app.listen(7000);
